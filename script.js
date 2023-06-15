011285286c92755b3acd79965af6dd71d14622db
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-15 19:09:22 +0300

    performed

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ document.addEventListener("keydown", (e) => {
   } else if (e.key.toLowerCase() === "e") {
     e.preventDefault();
     scrollToElement("e");
+  } else if (e.key.toLowerCase() === "g") {
+    e.preventDefault();
+    scrollToElement("g");
   }
 });
 
@@ -30,3 +33,42 @@ function scrollToElement(elementId) {
 function toggleAnimation(element) {
   element.classList.toggle("animate");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var request = new XMLHttpRequest();
+  var username = "<RudenkoAnna>";
+  var url = "https://api.github.com/users/RudenkoAnna/repos";
+  var projectsSection = document.getElementById("projects-section");
+  var projectsList = document.createElement("ul");
+  projectsList.id = "projects-list";
+
+  projectsSection.appendChild(projectsList);
+
+  request.open("GET", url, true);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      var repositories = JSON.parse(request.responseText);
+
+      repositories.forEach(function (repo) {
+        var listItem = document.createElement("li");
+        var link = document.createElement("a");
+        link.href = repo.html_url;
+        link.textContent = repo.full_name;
+
+        listItem.appendChild(link);
+
+        if (repo.description) {
+          var description = document.createElement("p");
+          description.textContent = repo.description;
+          listItem.appendChild(description);
+        }
+
+        projectsList.appendChild(listItem);
+      });
+    } else {
+      console.error("Error: " + request.status);
+    }
+  };
+
+  request.send();
+});
